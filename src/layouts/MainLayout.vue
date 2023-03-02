9a8fea78de9af1c46fb79ad573c863cbd86df25b
Leandro Henrique <emtudo@gmail.com>
2023-03-01 21:53:55 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Palavra Tempo
+        </q-toolbar-title>
+
+        <div>v 0.0.0</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+    return {
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn class="col-4" :label="`Palavra: ` + lugar" color="primary" @click="novoLugar"></q-btn>
+    <q-btn class="col-4" :label="`Letra: ` + letra" color="primary" @click="novaLetra"></q-btn>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z']
+    const lugares = [
+      'shopping',
+      'casa',
+      'escola',
+      'parque',
+      'mercado',
+      'padaria',
+      'farmácia',
+      'supermercado',
+      'restaurante',
+      'cinema',
+      'igreja',
+      'hospital'
+    ]
+    const novoLugar = () => {
+      lugar.value = lugares[Math.floor(Math.random() * lugares.length)]
+    }
+
+    const novaLetra = () => {
+      letra.value = letras[Math.floor(Math.random() * letras.length)]
+    }
+
+    const letra = ref(letras[Math.floor(Math.random() * letras.length)])
+    const lugar = ref(lugares[Math.floor(Math.random() * lugares.length)])
+
+    return {
+      novaLetra,
+      novoLugar,
+      letra,
+      lugar
+    }
+  }
+})
+</script>
